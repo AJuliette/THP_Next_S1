61d9445ffd6f41aedee81010f6a442eee60e8c19
Juliette Audema <juliette.audema@gmail.com>
2019-01-18 15:20:24 +0100

    Config sidekiq in production and correct some rubocop

--- spec/factories/categorizations.rb
+++ spec/factories/categorizations.rb
@@ -5,8 +5,8 @@
 # Table name: categorizations
 #
 #  id          :bigint(8)        not null, primary key
-#  item_id     :bigint(8)        not null
-#  category_id :bigint(8)        not null
+#  item_id     :bigint(8)
+#  category_id :bigint(8)
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
