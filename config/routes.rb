fa579de187ec9acad48f8feeae0188e283bd2806
Juliette Audema <juliette.audema@gmail.com>
2019-01-18 14:34:52 +0100

    Add mailjet in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  if Rails.env.production?
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(Rails.credentials.SIDEKIQ_USERNAME)) &
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(Rails.credentials.SIDEKIQ_PASSWORD))
+    end
+  end
+  mount Sidekiq::Web => '/sidekiq'
+
   root 'administration/items#index'
 
   get '/home', to: 'home#landing_page'
@@ -21,11 +30,5 @@ Rails.application.routes.draw do
     unlocks: 'users/unlocks'
   }
 
-  devise_for :admin, controllers: {
-    sessions: 'admins/sessions',
-    confirmations: 'admins/confirmations',
-    registrations: 'admins/registrations',
-    passwords: 'admins/passwords',
-    unlocks: 'admins/unlocks'
-  }
+  devise_for :admin, controllers: { sessions: 'admins/sessions', confirmations: 'admins/confirmations', registrations: 'admins/registrations', passwords: 'admins/passwords', unlocks: 'admins/unlocks' }
 end
