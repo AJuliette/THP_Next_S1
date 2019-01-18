cf034585c674c802834c682bbb543cc081faca7e
Juliette Audema <juliette.audema@gmail.com>
2019-01-18 15:11:59 +0100

    Correct the routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   if Rails.env.production?
     Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(Rails.credentials.SIDEKIQ_USERNAME)) &
-        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(Rails.credentials.SIDEKIQ_PASSWORD))
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(Rails.application.credentials.SIDEKIQ_USERNAME)) &
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(Rails.application.credentials.SIDEKIQ_PASSWORD))
     end
   end
   mount Sidekiq::Web => '/sidekiq'
